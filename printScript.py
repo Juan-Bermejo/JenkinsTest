0808d406519b2f124bdd3a5e57bb82eb4717f0c6
Juan-Bermejo <ignacio@bermejo.com>
2022-07-26 02:00:15 -0300

    Update printScript.py

--- printScript.py
+++ printScript.py
@@ -1,5 +1,4 @@
 import random
 
-print('Testing jenkins job with git repo')
-print('Add change to test the pull req in jenkins')
+print('--- Testing jenkins job with git repo ---')
 print(random.randint(0,1000))
